1a6dd75a742304390d2efd425ace15bb7127dcde
Tomer Fishaimer <tomer.fishaimer@aquasec.com>
2022-12-08 14:50:12 +0200

    Added algolia search to main page

--- src/components/Categories/CategoryItem.js
+++ src/components/Categories/CategoryItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Link from '@docusaurus/Link';
 
 export default ({ title, subtitle, icon, link }) => (
-    <Link className='rounded-[10px] bg-white shadow-lg h-45 flex flex-col items-center justify-center font-semibold text-blue-dark hover:text-blue-dark hover:no-underline'
+    <Link className='category-item rounded-[10px] bg-white shadow-lg h-45 flex flex-col items-center justify-center font-semibold text-blue-dark hover:text-blue-dark hover:no-underline'
         to={link}>
         <img src={`/img/${icon}.png`} />
         <div className='flex flex-col text-center mt-4'>
--- src/components/HomepageHeader/index.js
+++ src/components/HomepageHeader/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { DocSearch } from '@docsearch/react';
 import Combobox from '../Combobox';
 
 
@@ -8,7 +9,14 @@ export default function HomepageHeader() {
       <div className='text-2xl font-bold '>
         Welcome to <span className=' text-blue-300'>Aqua Platform Documentation</span>
       </div>
-      <input className='bg-white text-dark-primary w-[690px] h-10 py-1 px-4 rounded-md mt-4 font-medium text-base focus:ring-0 focus:outline-none' placeholder='Search...' />
+      <div className='mt-3'>
+
+        <DocSearch
+          appId="YN1Z4L0NG9"
+          indexName="aqua-test"
+          apiKey="7aa3cb124e2b876a3a82f5ec2741cc21"
+        />
+      </div>
       <div className=' absolute left-6 top-6 z-10'>
         <Combobox />
 
