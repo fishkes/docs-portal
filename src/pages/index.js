cba75d42e4e75d69689bafc522c2ab3a7e0f5d2a
Tomer Fishaimer <tomer.fishaimer@aquasec.com>
2022-12-08 11:35:12 +0200

    Fixed a typo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,16 @@
 import React from 'react';
-import BrowserOnly from '@docusaurus/BrowserOnly';
-import Topnav from '@site/src/components/Topnav';
+import TopNav from '@site/src/components/TopNav';
 import HomepageHeader from '@site/src/components/HomepageHeader'
 import Categories from '@site/src/components/Categories'
 
 export default function Home() {
   return (
     <div className='font-inter'>
-      <Topnav />
+      <TopNav />
       <HomepageHeader />
       <main>
         <Categories />
       </main>
     </div>
-    // <BrowserOnly>
-    //   {() =>
-    //   (<div className='font-inter'>
-    //     <Topnav />
-    //     <HomepageHeader />
-    //     <main>
-    //       <Categories />
-    //     </main>
-    //   </div>)}
-    // </BrowserOnly>
   );
 }
